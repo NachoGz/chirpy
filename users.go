12a8084ddad055e963a03600f0179acd63fa1952
NachoGz <ignaciogomezf1@gmail.com>
2025-02-06 10:26:37 -0300

    add handler for /api/users endpoint

---
+++ users.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	// "log"
+)
+
+func (cfg *apiConfig) handlerUserCreation(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	user, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating user", err)
+		return
+	}
+	new_user := User{
+		ID: user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email: user.Email,
+	}
+	// log.Printf((user.Email))
+	respondWithJSON(w, http.StatusCreated, new_user)
+}
