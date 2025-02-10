d27fa7bb33140341fb398fe209db53a3eeb3b0a8
NachoGz <ignaciogomezf1@gmail.com>
2025-02-10 09:14:46 -0300

    add queries for changing email and password, get user by id and upgrade to chirpy red

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,11 +1,12 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email, hashed_password)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password, is_chirpy_red)
 VALUES (
     gen_random_uuid(),
     NOW(),
     NOW(),
     $1,
-    $2
+    $2,
+    $3
 )
 RETURNING *;
 
@@ -15,3 +16,18 @@ DELETE FROM users;
 -- name: GetUserByEmail :one
 SELECT * FROM users
 WHERE email=$1;
+
+-- name: GetUserByID :one
+SELECT * FROM users
+WHERE id=$1;
+
+-- name: ChangeEmailAndPassword :one
+UPDATE users
+SET email = $2, hashed_password = $3, updated_at = NOW()
+WHERE id = $1
+RETURNING *;
+
+-- name: UpgradeToChirpyRed :exec
+UPDATE users
+SET is_chirpy_red = TRUE, updated_at = NOW()
+WHERE id = $1;
